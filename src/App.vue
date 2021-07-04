7c2f8b2d28798f38646efdd795afaa194521a3b4
EC2 Default User <ec2-user@ip-172-31-40-95.ap-northeast-1.compute.internal>
2021-07-04 03:49:52 +0000

    pucture up

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<v-container fluid fill-height>
+  <v-layout align-center justify-center>
+    <div v-if="signedIn">
+      <amplify-photo-picker :photo-picker-config="photoPickerConfig" />
+      <amplify-s3-album path="upload/"></amplify-s3-album>
+      <amplify-sign-out />
+    </div>
+    <div v-else>
+      <amplify-sign-out />
+    </div>
+  </v-layout>
+</v-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { AmplifyEventBus } from 'aws-amplify-vue'
+import Auth from '@aws-amplify/auth'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  props: [],
+  data () {
+    return {
+      signedIn: false,
+      photoPickerConfig: {
+        header: 'Upload Profile Pic',
+        accept: 'image/*',
+        path: 'upload/'
+      }
+    }
+  },
+  async beforeCreate () {
+    try {
+      await Auth.currentAuthenticatedUser()
+      this.signedIn = true
+    } catch (err) {
+      this.signedIn = false
+      console.log(err)
+    }
+    AmplifyEventBus.$on('fileUpload', (img) => {
+      console.log('img:' + JSON.stringify(img))
+    })
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
